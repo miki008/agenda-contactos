35abd59e2fee97b5081d47e410c88622b2b22e24
Miki008 <mikiescola@gmail.com>
2025-12-08 11:47:02 +0100

    agenda_video009

--- funciones.js
+++ funciones.js
@@ -15,3 +15,13 @@ function obtenerDatosHTML(nombre) {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 
 }
+
+
+function obtenerContactos(){
+    let hoja = SpreadsheetApp.openById('14qFioWE5iplfM3aNSgctBvsin6kQz4H0vacB5V1337M').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    
+    datos.shift();
+    Logger.log(datos);
+    return datos;
+}
